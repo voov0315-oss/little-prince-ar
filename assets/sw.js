b13b932558974ab876a16f2b633e07f68f488679
voov0315-oss <voov0315@gmail.com>
2026-09-10 02:27:07 +0900

    Add files via upload

---
+++ assets/sw.js
@@ -0,0 +1,1 @@
+const CACHE='little-prince-ar-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/six-zone-markers.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
